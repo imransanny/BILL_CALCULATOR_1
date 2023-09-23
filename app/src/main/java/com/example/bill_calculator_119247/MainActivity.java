ccb7f32c6c97551c7810350bd44db2ce829ed543
imran619 <imransanny619@gmail.com>
2023-09-23 11:52:10 +0600

    1st update

---
+++ app/src/main/java/com/example/bill_calculator_119247/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bill_calculator_119247;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
